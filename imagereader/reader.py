ce05c848e17a835b86799afc91d9fb69bed35d29
HolyGuacamole <HolyGuacamole@users.noreply.github.com>
2017-04-25 12:36:16 +0200

    made reader iteration over cases explicitly

--- imagereader/reader.py
+++ imagereader/reader.py
@@ -20,10 +20,19 @@ def find_dicom_series(root_dir, key, value):
     this series is relevant. If so, load entire directory, if not, move to next directory"""
 
     dicom_series = []
-    sub_dirs = [x[0] for x in os.walk(root_dir)]  # Gather all subdirectories in 'root_dir'
-    for directory in sub_dirs:
-        file = glob.glob(directory + '/*.dcm')[0]  # Checking just one .dcm file is sufficient
+    case_dirs = [os.path.join(root_dir, x) for x in os.listdir(root_dir)]
+    
+    for case_dir in case_dirs:
+        if len(os.listdir(case_dir)) != 1:
+            print("Unexpectedly found multiple folders for case", case_dir)
+            print("Skipping this case for now.")
+            continue
+        else:
+            # the 298318301283.129392381.1392892.1923891 folder name
+            case_dir = os.path.join(case_dir, os.listdir(case_dir)[0])
+        
+        file = glob.glob(case_dir + '/*.dcm')[0]  # Checking just one .dcm file is sufficient
         img = SimpleITK.ReadImage(file)  # Read single .dcm file to obtain metadata
         if value in img.GetMetaData(key):  # Check whether metadata key contains the right value
-            dicom_series.append(load_dicom_series(directory))  # Loads all slices at once
+            dicom_series.append(load_dicom_series(case_dir))  # Loads all slices at once
     return dicom_series
