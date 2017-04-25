ff17982364fa5be0c54916bdc47b61c6e79e7808
Jeftha Spunda <jspunda@gmail.com>
2017-04-25 12:30:47 +0200

    Fix for dirs without .dcm files

--- imagereader/reader.py
+++ imagereader/reader.py
@@ -22,8 +22,17 @@ def find_dicom_series(root_dir, key, value):
     dicom_series = []
     sub_dirs = [x[0] for x in os.walk(root_dir)]  # Gather all subdirectories in 'root_dir'
     for directory in sub_dirs:
-        file = glob.glob(directory + '/*.dcm')[0]  # Checking just one .dcm file is sufficient
-        img = SimpleITK.ReadImage(file)  # Read single .dcm file to obtain metadata
-        if value in img.GetMetaData(key):  # Check whether metadata key contains the right value
-            dicom_series.append(load_dicom_series(directory))  # Loads all slices at once
+        file_list = glob.glob(directory + '/*.dcm')  # Look for .dcm files
+        if file_list:  # If we find a dir with a .dcm series, process it
+            dcm_file = file_list[0]  # Checking just one .dcm file is sufficient
+            img = SimpleITK.ReadImage(dcm_file)  # Read single .dcm file to obtain metadata
+            if value in img.GetMetaData(key):  # Check whether metadata key contains the right value
+                dicom_series.append(load_dicom_series(directory))  # Loads all slices at once
     return dicom_series
+
+# Example usage
+# rootdir = 'C:/Users/Jeftha/Downloads/DOI/'
+# key = '0008|103e'
+# value = 'ADC'
+#
+# find_dicom_series(rootdir, key, value)
