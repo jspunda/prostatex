d36bcb3e144a8e3209617684b294f2959b3fe4fe
HolyGuacamole <HolyGuacamole@users.noreply.github.com>
2017-04-25 14:52:10 +0200

    reader now takes dictionary as input, made reader print some text when encountering unexpected situation (files/folders missing, more/less scans than expected, etc.), also added method for loading ADC scans.

--- imagereader/reader.py
+++ imagereader/reader.py
@@ -12,27 +12,57 @@ def load_dicom_series(input_dir):
     dicom_series = reader.Execute()
     return SimpleITK.GetArrayFromImage(dicom_series)
 
-
-def find_dicom_series(root_dir, key, value):
-    """Returns all DICOM series as pixel data in 'root_dir' and its sub directories in which
-    the metadata key 'key' has value 'value'. Since DICOM metadata is lost when reading
-    all slices at once, we first check a single .dcm slice to determine whether
+def check_scan_metadata(scan_directory, expected_metadata):
+    scan_files = glob.glob(scan_directory + '/*.dcm')
+    
+    # Read single .dcm file to obtain metadata
+    img = SimpleITK.ReadImage(scan_files[0])
+    
+    for (key, value) in expected_metadata.items():
+        # Check whether metadata key contains the right value
+        if value not in img.GetMetaData(key):
+            return False
+    return True
+    
+def find_ADC_dicom_series(dir):
+    metadata = {'0008|103e':'ADC'}
+    return find_dicom_series(dir, metadata)
+    
+def find_dicom_series(root_dir, expected_metadata):
+    """Returns the DICOM series for scans that have the expected metadata
+    for all the cases in 'root_dir' (i.e. the 'DOI' folder). 
+    Since DICOM metadata is lost when reading all slices at once, 
+    we first check a single .dcm slice to determine whether
     this series is relevant. If so, load entire directory, if not, move to next directory"""
 
     dicom_series = []
     case_dirs = [os.path.join(root_dir, x) for x in os.listdir(root_dir)]
     
+    print("Found", len(case_dirs), "case(s).")
+    
     for case_dir in case_dirs:
-        if len(os.listdir(case_dir)) != 1:
+        # Expecting that each case folder (e.g. "ProstateX-0000") contains
+        # one folder with a 'numbers' name (e.g. "1.3.6.1.4.1.14519.5.2.1.7311.5101.158323547117540061132729905711")
+        subdirs = os.listdir(case_dir)
+        if len(subdirs) != 1:
             print("Unexpectedly found multiple folders for case", case_dir)
             print("Skipping this case for now.")
             continue
-        else:
-            # the 298318301283.129392381.1392892.1923891 folder name
-            case_dir = os.path.join(case_dir, os.listdir(case_dir)[0])
+
+        case_dir = os.path.join(case_dir, subdirs[0])
+        scan_dirs = [os.path.join(case_dir, x) for x in os.listdir(case_dir)]
         
-        file = glob.glob(case_dir + '/*.dcm')[0]  # Checking just one .dcm file is sufficient
-        img = SimpleITK.ReadImage(file)  # Read single .dcm file to obtain metadata
-        if value in img.GetMetaData(key):  # Check whether metadata key contains the right value
-            dicom_series.append(load_dicom_series(case_dir))  # Loads all slices at once
+        scan_found = False
+        
+        for scan_dir in scan_dirs:
+            if check_scan_metadata(scan_dir, expected_metadata):
+                # Loads all slices at once
+                dicom_series.append(load_dicom_series(scan_dir))  
+                
+                if scan_found:
+                    print("Found another scan with matching metadata.", scan_dir)
+                scan_found = True
+        
+        if not scan_found:
+            print ("Could not find a scan for case", case_dir)
     return dicom_series
