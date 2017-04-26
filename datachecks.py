83f3727efc0a5cc86b32d49278cb7426e858d52d
HolyGuacamole <HolyGuacamole@users.noreply.github.com>
2017-04-26 23:07:35 +0200

    added a data check, not tested on full data set.

---
+++ datachecks.py
@@ -0,0 +1,28 @@
+import seriesreader.reader as reader
+
+DATA_DIR = 
+
+def check_equal_voxel_size(metadata):
+    series_paths = reader.find_dicom_series_paths(DATA_DIR, metadata)
+    if reader.check_equal_voxel_size(series_paths):
+        print("The scans have the same voxel size")
+    else:
+        print("Found different voxel sizes for the scans with the following metadata:", metadata)
+
+"""
+t2_tse_tra
+ep2d_diff_tra_DYNDIST
+t2_tse_sag
+ep2d_diff_tra_DYNDISTCALC_BVAL
+ep2d_diff_tra_DYNDIST_ADC
+t2_tse_cor
+tfl_3d PD ref_tra_1.5x1.5_t3
+"""    
+
+check_equal_voxel_size({'0008|103e':'ADC'})
+check_equal_voxel_size({'0008|103e':'t2_tse_tra'})
+check_equal_voxel_size({'0008|103e':'t2_tse_sag'})
+check_equal_voxel_size({'0008|103e':'BVAL'})
+check_equal_voxel_size({'0008|103e':'cor'})
+check_equal_voxel_size({'0008|103e':'tfl_3d'})
+check_equal_voxel_size({'0008|103e':'cor'})
