b37ca766d62a2bb3e01080adfaa91dacad660248
Jeftha Spunda <jspunda@gmail.com>
2017-05-13 16:54:41 +0200

    Added support for windowing

--- data_visualization/visualize.py
+++ data_visualization/visualize.py
@@ -10,16 +10,18 @@ def require_dir(path):
         os.makedirs(path)
 
 
-def visualize_lesions(lesions, references, lesion_info, save=False):
+def visualize_lesions(lesions, references, lesion_info, save=False, window=(None, None)):
     """
     Visualizes lesion cutouts, their histograms and a bigger reference image. 
     If save=True, plots will instead be saved to disk to img/{TRUE, FALSE}, depending on the lesion truth label
-    
+
     :param lesions: Small lesion cutout image
     :param references: Bigger reference image for the lesion cutout
     :param lesion_info: Lesion attributes (fid, zone, clinsig, etc.)
     :param save: Indicates whether plots should be shown on screen or saved to disk
-    
+    :param window: Min and max window values when plotting images, such that every pixel < min becomes min and 
+    every pixel > max becomes max
+
     Note: For ADC images, saving all plots (334 in total) takes a few minutes.
     """
     # Flatten image for histogram plot
@@ -32,13 +34,15 @@ def visualize_lesions(lesions, references, lesion_info, save=False):
         require_dir('img/FALSE')
 
     for i in range(lesions_flat.shape[0]):
+        # Global title
+        plt.suptitle('Lesion and histogram (window {} - {})'.format(window[0], window[1]), y=1)
         # Create axes on a plot grid
         ax1 = plt.subplot2grid((2, 2), (0, 0))
         ax2 = plt.subplot2grid((2, 2), (0, 1))
         ax3 = plt.subplot2grid((2, 2), (1, 0), colspan=2)
 
         # First ax contains lesion cutout
-        ax1.imshow(lesions[i], cmap='gray')
+        ax1.imshow(lesions[i], cmap='gray', vmin=window[0], vmax=window[1])
 
         # Extract lesion attributes to display in title
         parsed_name = lesion_info[i]['name'].split('/')
@@ -54,7 +58,7 @@ def visualize_lesions(lesions, references, lesion_info, save=False):
                       .format(patient_id, dcm_series, fid, zone, age, clinsig))
 
         # Plot reference image for lesion
-        ax2.imshow(references[i], cmap='gray')
+        ax2.imshow(references[i], cmap='gray', vmin=window[0], vmax=window[1])
         ax2.set_title('Reference image')
 
         # Create histogram for lesion cutout
@@ -82,4 +86,4 @@ if __name__ == "__main__":
     query_words = ['ADC']
     X, y = get_train_data(h5_file, query_words, keep_lesion_data=True, size_px=6)
     X_big, y = get_train_data(h5_file, query_words, keep_lesion_data=True, size_px=40)
-    visualize_lesions(X, X_big, y, save=False)
+    visualize_lesions(X, X_big, y, save=False, window=(300, 800))
