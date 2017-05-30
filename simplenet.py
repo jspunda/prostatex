56c11d311fc136e3c07bd88754e78cc25bf5ca3a
kbasten <kbasten@gmail.com>
2017-05-30 15:29:31 +0200

    model to module

--- simplenet.py
+++ simplenet.py
@@ -16,52 +16,59 @@ from utils.auc_callback import AucHistory
 from utils.generator_from_config import get_generator
 from utils.train_test_split import train_test_split
 
-AUGMENTATION_CONFIGURATION = 'baseline'
 
-## Model
-model = Sequential()
-model.add(Conv2D(32, (3, 3), activation=LeakyReLU(), kernel_initializer='he_normal', bias_initializer=RandomNormal(mean=0.1), input_shape=(16, 16, 1)))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+def get_model(configuration='baseline'):
+    AUGMENTATION_CONFIGURATION = configuration
 
-model.add(Conv2D(64, (2, 2), activation=LeakyReLU(), kernel_initializer='he_normal', bias_initializer=RandomNormal(mean=0.1)))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    ## Model
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation=LeakyReLU(), kernel_initializer='he_normal', bias_initializer=RandomNormal(mean=0.1), input_shape=(16, 16, 1)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (2, 2), activation=LeakyReLU(), kernel_initializer='he_normal', bias_initializer=RandomNormal(mean=0.1)))
-model.add(BatchNormalization())
-model.add(Conv2D(64, (2, 2), activation=LeakyReLU(), kernel_initializer='he_normal', bias_initializer=RandomNormal(mean=0.1)))
-model.add(BatchNormalization())
+    model.add(Conv2D(64, (2, 2), activation=LeakyReLU(), kernel_initializer='he_normal', bias_initializer=RandomNormal(mean=0.1)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (1, 1), activation=LeakyReLU(), kernel_initializer='he_normal', bias_initializer=RandomNormal(mean=0.1)))
-model.add(BatchNormalization())
+    model.add(Conv2D(64, (2, 2), activation=LeakyReLU(), kernel_initializer='he_normal', bias_initializer=RandomNormal(mean=0.1)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (2, 2), activation=LeakyReLU(), kernel_initializer='he_normal', bias_initializer=RandomNormal(mean=0.1)))
+    model.add(BatchNormalization())
 
-model.add(Conv2D(1, (1, 1), activation='sigmoid'))
-model.add(Flatten())
+    model.add(Conv2D(64, (1, 1), activation=LeakyReLU(), kernel_initializer='he_normal', bias_initializer=RandomNormal(mean=0.1)))
+    model.add(BatchNormalization())
 
-# For a binary classification problem
-sgd = SGD(lr=0.0005, momentum=0.9)
-model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
+    model.add(Conv2D(1, (1, 1), activation='sigmoid'))
+    model.add(Flatten())
 
-## Data
-h5_file_location = os.path.join('/scratch-shared/ISMI/prostatex', 'prostatex-train.hdf5')
-h5_file = h5py.File(h5_file_location, 'r')
-train_data_list, train_labels_list, attr = get_train_data(h5_file, ['ADC'])
+    # For a binary classification problem
+    sgd = SGD(lr=0.0005, momentum=0.9)
+    model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
 
-train_data, val_data, train_labels, val_labels = train_test_split(train_data_list, train_labels_list, attr, test_size=0.33)
+    ## Data
+    h5_file_location = os.path.join('/scratch-shared/ISMI/prostatex', 'prostatex-train.hdf5')
+    h5_file = h5py.File(h5_file_location, 'r')
+    train_data_list, train_labels_list, attr = get_train_data(h5_file, ['ADC'])
 
-train_data = np.expand_dims(train_data, axis=-1)
-val_data = np.expand_dims(val_data, axis=-1)
-train_labels = np.expand_dims(train_labels, axis=-1)
-val_labels = np.expand_dims(val_labels, axis=-1)
+    train_data, val_data, train_labels, val_labels = train_test_split(train_data_list, train_labels_list, attr, test_size=0.33)
 
-## Stuff for training
-generator = get_generator(configuration=AUGMENTATION_CONFIGURATION)
+    train_data = np.expand_dims(train_data, axis=-1)
+    val_data = np.expand_dims(val_data, axis=-1)
+    train_labels = np.expand_dims(train_labels, axis=-1)
+    val_labels = np.expand_dims(val_labels, axis=-1)
 
-train_generator = generator.flow(train_data, train_labels)  #, save_to_dir="/nfs/home4/schellev/augmented_images")
-batch_size = 128
-steps_per_epoch = len(train_labels_list) // batch_size
+    ## Stuff for training
+    generator = get_generator(configuration=AUGMENTATION_CONFIGURATION)
 
-auc_history = AucHistory(train_data, train_labels, val_data, val_labels, output_graph_name=AUGMENTATION_CONFIGURATION)
+    train_generator = generator.flow(train_data, train_labels)  #, save_to_dir="/nfs/home4/schellev/augmented_images")
+    batch_size = 128
+    steps_per_epoch = len(train_labels_list) // batch_size
 
-model.fit_generator(train_generator, steps_per_epoch, epochs=100, verbose=2, callbacks=[auc_history], max_q_size=50, workers=8)
+    auc_history = AucHistory(train_data, train_labels, val_data, val_labels, output_graph_name=AUGMENTATION_CONFIGURATION)
+
+    model.fit_generator(train_generator, steps_per_epoch, epochs=100, verbose=2, callbacks=[auc_history], max_q_size=50, workers=8)
+
+    return model
+
+if __name__ == "__main__":
+    m = get_model()
